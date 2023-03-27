5a39870cdd70c12e3579d3f6a24009c40d418927
Dovlat <dovlat@catacloud.no>
2023-03-27 13:44:29 +0200

    adds github loader locally

--- scripts/ingest.ts
+++ scripts/ingest.ts
@@ -1,10 +1,10 @@
-import { GithubRepoLoader } from "langchain/document_loaders";
 import { SupabaseVectorStore } from "langchain/vectorstores";
 import { OpenAIEmbeddings } from "langchain/embeddings";
 import { createClient } from "@supabase/supabase-js";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { config } from "dotenv";
 import path from "path";
+import { GithubRepoLoader } from "./base";
 
 config({ path: path.resolve(process.cwd(), ".env.local") });
 
@@ -12,9 +12,10 @@ async function run() {
   try {
     const loader = new GithubRepoLoader(
       "https://github.com/glasserviceoslo/glass.no",
+      // "https://github.com/davlet61/powerofficesuiteconnector",
       {
         branch: "netlify",
-        recursive: false,
+        recursive: true,
         unknown: "warn",
         accessToken: process.env.GITHUB_ACCESS_TOKEN,
       }
@@ -27,19 +28,25 @@ async function run() {
 
     const docs = await loader.loadAndSplit(textSplitter);
 
+    const CHUNK_SIZE = 50;
+    const chunks = Array.from(
+      { length: Math.ceil(docs.length / CHUNK_SIZE) },
+      (_, index) => docs.slice(index * CHUNK_SIZE, (index + 1) * CHUNK_SIZE)
+    );
+
     const client = createClient(
       process.env.SUPABASE_URL || "",
       process.env.SUPABASE_PRIVATE_KEY || ""
     );
 
-    await SupabaseVectorStore.fromDocuments(
-      docs,
-      new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
-      {
-        client,
-        tableName: "documents",
-      }
-    );
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
+    const dbOpts = { client, tableName: "documents" };
+
+    for (const chunk of chunks) {
+      await SupabaseVectorStore.fromDocuments(chunk, embeddings, dbOpts);
+    }
   } catch (error: any) {
     console.log("error =>>>>> ", error.message);
     console.log("Failed to ingest your data");
